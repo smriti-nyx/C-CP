da0e36f298995f91e894ee4e96aca4fb63b6eac9
smriti-nyx <smriticodedthis@gmail.com>
2026-01-15 23:46:01 +0530

    learning c++

---
+++ hello.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+int main() {
+
+
+    
+//single line comments 
+/*multiple line
+comments use a slash and an
+asterisk */
+
+//variables 
+
+int x ;  //declarartion
+x = 5; //assignment
+std::cout << x; 
+return 0;
+}
+
