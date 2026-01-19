10a03143506d1e6efee885d3f1f65b334ac1a4c0
smriti-nyx <smriticodedthis@gmail.com>
2026-01-20 01:13:58 +0530

    using command

--- tut2c++.cpp
+++ tut2c++.cpp
@@ -6,22 +6,14 @@ using namespace std;
   
  
   int main() {
-     //typedef - reserved keyword used to create additional name (alias) for another data type
-//          new identifier for a  new data type 
-//           helps with readibility and reduces typos 
-
-// using command 
+ 
+//using - used as an alternative to typedef coz it is also easier to work with templates 
 
 
- // typedef std::vector < std::pair< std:: string << int > pairlist_t ;// _t is a default way to name new data type names. 
-typedef std:: string txt_t;
-  typedef int num_t;
-  num_t age = 24;
-  txt_t name = "Sera";
-  cout << age << "\n";
-  cout << "My name is " << name << "\n";
-  
-  
+using txt_t = std:: string;
+txt_t state = "Ohio";
+cout << " I was born and brought up in " << state<< "\n";
+
   
   
 
