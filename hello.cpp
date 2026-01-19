062b3e4fd3d79182261e7acb474860e0ea9ea1dc
smriti-nyx <smriticodedthis@gmail.com>
2026-01-19 21:31:51 +0530

    variables done and diff data types

--- hello.cpp
+++ hello.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+using namespace std;
 int main() {
 
 
@@ -12,7 +13,24 @@ asterisk */
 
 int x ;  //declarartion
 x = 5; //assignment
-std::cout << x; 
+
+ // double(number including decimal)
+  double price = 29.0;
+  double initial = 3.5;
+  double gpa = 9.8;
+
+  // single character
+  char grade = 'A';
+  // char can store only one character
+
+  // strings are like characters but can store more tha one charcter
+  std::string name = "Sera";
+  std::string city = "London";
+  cout << "Helllo " << name << "\n";
+  cout << "You are from " << city << "\n";
+  cout << x << "\n";
+
+
 return 0;
 }
 
