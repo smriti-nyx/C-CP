fb8fc707055ea3e6ebba89036855f89f2c1c2c5f
smriti-nyx <smriticodedthis@gmail.com>
2026-01-21 01:43:05 +0530

    Arithmetic operations

--- tut2c++.cpp
+++ tut2c++.cpp
@@ -14,9 +14,33 @@ using txt_t = std:: string;
 txt_t state = "Ohio";
 cout << " I was born and brought up in " << state<< "\n";
 
+  int students = 21;
+ // students = students +1;
+ // students+=1;
+ // students++;
+
+ // students = students -1;
+  //students-= 2;
+  //students --;
+
+ // students  = students *2;
+ // students *=4;
+
+  int bats = 4 ;
+  double remainder = bats % 2;
+  double remainder1 = bats % 3;
+  
+  cout << students <<"\n";
+cout << remainder<<"\n";
+  cout << remainder1<<"\n";
+
   
 //priority  is parenthesis > multiplication or division > addition and subtraction.
   
+//Arithmetic operators 
+
+//Addition
+
   
   
 
