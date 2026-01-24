9392c0fd8d4b5f8f31252acdb680f04dc7b01ae2
Seraphine Woods <smriticodedthis@gmail.com>
2026-01-24 23:06:44 +0530

    Edited

--- Tut4c++.cpp
+++ Tut4c++.cpp
@@ -8,6 +8,23 @@ int main(){
  
   
   
+//logical operators 
+  //&& = check if two conditions are true 
+  // || = check  if atleast one of the two conditions is true 
+  //!=  reverses the logical  state of its operand  .
 
+  int temp;
+  cout <<"Enter the temperature: \n";
+  cin>> temp;
+  if (temp > 0 && temp < 30){
+    cout<<"The temperature is good \n";
+    
+  }
+  else {
+    cout<< "The temperature is not good\n";
+    
+  }
+
+  
 return 0;
 }
