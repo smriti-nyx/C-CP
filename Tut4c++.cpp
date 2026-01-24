40252339cf3948954498b53088dea7658490f0f3
Seraphine Woods <smriticodedthis@gmail.com>
2026-01-25 04:35:19 +0530

    edited

--- Tut4c++.cpp
+++ Tut4c++.cpp
@@ -25,6 +25,16 @@ int main(){
     
   }
 
+    std:: string name;
+
+ cout<<"What is your name ? \n";
+  std::getline(std::cin , name); // reads the words after spaces too.
   
+  cin>> name;
+
+  cout<< " hey "<< name <<"\n";
+  
+
+
 return 0;
 }
