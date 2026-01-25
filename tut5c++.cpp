31ed3fc1c01933644cffe54ce54c7c5d0d147a85
Seraphine Woods <smriticodedthis@gmail.com>
2026-01-26 00:42:30 +0530

    edited

--- tut5c++.cpp
+++ tut5c++.cpp
@@ -18,6 +18,51 @@ int main(){
     cout<<"Happy new year \n";
 
   }
+
+
+  //break = break out of a loop
+  //continue = skip current iteration.
+
+  for(int i = 1 ; i<=20; i++){
+    if(i ==13){
+    break;
+  }
+  cout<<i<<"\n";
+
+  }
+
+
+  for(int i = 1 ; i<=20; i++){
+    if(i ==13){
+    continue;
+  }
+  cout<<i<<"\n";
+
+  }
+
+
+  //nested loops 
+  int columns ;
+  int rows;
+  char symbol;
+  cout<<"Enter the no. of columns : \n";
+  cin>> columns;
+
+  cout<<"Enter the no. of rows : \n";
+  cin>> rows;
+
+  cout<<"Enter the symbol: \n";
+  cin>> symbol;
+
+  for( int i =1 ; i <= rows; i++){
+    for(int j = 1; j<= columns;j++){
+      cout<<symbol;
+    }
+    cout<<symbol <<"\n";
+  }
+
+
+
   
 
 
