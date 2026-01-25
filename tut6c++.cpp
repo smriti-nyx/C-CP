d0831ad90344485dbbde9dda34b9cdd66a4e757c
Seraphine Woods <smriticodedthis@gmail.com>
2026-01-26 00:51:42 +0530

    edited

---
+++ tut6c++.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+void hbd(){
+  cout<<"Happy birthday to you \n";
+  cout<<"Happy birthday to you \n";
+  cout<<"May god bless you\n\n"; //pay attendtion to extra line character here for creating a new para when  //the fucntion is recalled
+}
+
+
+int main(){
+// function- a block of reusable code
+  
+  
+hbd();
+  
+  hbd();
+  
+  hbd();
+
+
+
+    return 0;
+}
+
