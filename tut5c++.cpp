6ab9547cc590fcb542a3264758057b33f7ae8a67
Seraphine Woods <smriticodedthis@gmail.com>
2026-01-26 00:09:12 +0530

    edited

---
+++ tut5c++.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    //while loops, do while loops 
+    std:: string name ;
+
+  while(name.length() < 12){
+    cout<<"Enter your name: \n";
+
+      cin>> name;
+  }
+  cout<<"Your name is "<< name<< "\n";
+
+
+
+  //for loops 
+  for (int i = 1; i<=10; i++){
+    cout<<"Happy new year \n";
+
+  }
+  
+
+
+  
+  
+return 0;
+
+}
