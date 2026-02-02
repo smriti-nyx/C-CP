630238f698126b29574f73125aa053c8892752ac
Seraphine Woods <smriticodedthis@gmail.com>
2026-02-02 23:56:19 +0530

    edited

--- tut8c++.cpp
+++ tut8c++.cpp
@@ -6,15 +6,15 @@ void sortarray(int a[], int size );
 int main(){
 
     int array[] = {1,2,3,4,5,6,7,8,9,10};
-    int size = sizeof(array)/sizeof(array[0]):
+    int size = sizeof(array)/sizeof(array[0]);
     
 
-    sort(array, size );
+    sortarray(array, size );
     for(int element : array){
         cout<<element<< " ";
     }
 
-    return 0 ;
+    return 0;
     
 }
 
@@ -22,10 +22,10 @@ void sortarray(int array[] , int size ){
     for(int i =0;i < size -1 ; i++){
         for( int j = 0; j < size - i -1 ; j++){
             if(array[j] > array[j+1]){
-                swap( array[j], array[j+1])
+                swap( array[j], array[j+1]);
             }
         }
     }
     
     }
-}
+
