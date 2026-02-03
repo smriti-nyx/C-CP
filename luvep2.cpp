9e3e62632ed44da5c68dcf1dde064729f6d52267
Seraphine Woods <smriticodedthis@gmail.com>
2026-02-03 21:42:45 +0530

    Edited

---
+++ luvep2.cpp
@@ -0,0 +1,30 @@
+
+
+
+    #include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+
+int a = 5;
+a = a +1 ;
+  cout<<a<<"\n";
+  cout<<a++<<"\n";
+   cout<<a<<"\n";
+   cout<<++a<<"\n";
+    cout<<a<<"\n";
+   cout<<a--<<"\n";
+    cout<<a<<"\n";
+   cout<<--a<<"\n";
+    cout<<a<<"\n";
+
+
+
+
+
+  return 0;
+}
+
+
+
