bc260c71817212204ea5374299e7417261ad6f79
Seraphine Woods <smriticodedthis@gmail.com>
2026-02-03 21:58:44 +0530

    edited

--- luvep2.cpp
+++ luvep2.cpp
@@ -20,6 +20,26 @@ a = a +1 ;
     cout<<a<<"\n";
 
 
+    //Typecasting - changing the data types
+
+
+    char a = 'a';
+char b = 'd';
+cout<<a<<"\n";
+cout<<(int) a <<"\n"; //TYPECASTED 
+cout<< a-b; //ASCII values will be printed
+
+
+
+//Taking inputs and outputs
+
+int x ;
+double y;
+char z ;
+cin>>x>>y>>z ;
+cout<<x<<" "<< y<<" "<<z<<"  \n";
+
+
 
 
 
